87575604ff4ed2434c162d698b3d34025c52bf6b
Konstantin Sidnikhin <sidnikhin@yandex.ru>
2021-12-03 15:47:26 +0400

    solved

--- Arr.php
+++ Arr.php
@@ -7,5 +7,13 @@ class Arr
     public function add($num){
         $this->nums[]=$num;
     }
+    public $sumHelper;
+    public function __construct(){
+       $this->sumHelper=new SumHelper;
+    }
+    public function getSum23(){
+        $nums=$this->nums;
+        return $this->sumHelper->getSum2($nums)+$this->sumHelper->getSum3($nums);
+    }
 
 }
